e14b5802fbf6161b8b8747a1ccde1d8908e13c3a
Jonathan McFadden <mcfaddja@uw.edu>
2018-08-10 17:18:04 -0700

    sockets server, initial commit - 2

---
+++ sockets/server/main.c
@@ -0,0 +1,27 @@
+//
+// Created by Jonathan McFadden on 8/10/18.
+//
+
+//#include "../shared/error.h"
+//#include "../shared/comm.h"
+//#include "../shared/commands.h"
+//#include "hashmap.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+//#include <pthread.h>
+//#include <stdint.h>
+
+#define MAX_BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]) {
+    
+}
+
