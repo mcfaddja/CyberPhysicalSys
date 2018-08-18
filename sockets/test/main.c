9e0991c614875c6f51e17704799e232ac003498c
Jonathan McFadden <mcfaddja@uw.edu>
2018-08-18 16:16:26 -0700

    testing work

--- sockets/test/main.c
+++ sockets/test/main.c
@@ -13,16 +13,21 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h>
+#include <inttypes.h>
+#include <math.h>
 
 
 int main(int argc, char *argv[])
 {
     int listenfd = 0, connfd = 0, n = 0;
     struct sockaddr_in serv_addr;
-    struct tm tm;
+    struct tm* tm;
+    time_t ticks;// = time(NULL);
 
     char buff[2048];
 
+    char timeBuff[32];
+//    memset(timeBuff, '\0', sizeof(timeBuff));
 //    FILE *dataFile;
 //    char fileBuff[1025];
 
@@ -45,11 +50,27 @@ int main(int argc, char *argv[])
 
     while(1)
     {
-        connfd = accept(listenfd, (struct sockaddr *) NULL, NULL);
+//      connfd = accept(listenfd, (struct sockaddr *) NULL, NULL);
+
+//	char timeBuff[100];
+//	memset(timeBuff, '\0', sizeof(timeBuff));
+//	time_t ticks = time(NULL);
+//	time_t nowTicks = ticks;
+//	snprintf(timeBuff, sizeof(timeBuff), "%.24s\n", ctime(&ticks));
+//	fprintf(stdout, timeBuff);
+//	printf("Time: %.24s\n", ctime(&ticks));
+	time(&ticks);
+	tm = localtime(&ticks);
+	strftime(timeBuff, 26, "%Y-%m-%d %H:%M:%S \r\n", tm);
+	puts(timeBuff);
+	
+	connfd = accept(listenfd, (struct sockaddr *) NULL, NULL);
 
         while ((n = read(connfd, buff, sizeof(buff) - 1)) > 0)
         {
-            fprintf(stdout, buff);
+            printf("%s \n %s", timeBuff, buff);
+//	    time_t ticks = time(NULL);
+//	    fprintf(stdout, "\n %.24s\r\n", ctime(&ticks));
         }
 
         puts("\n connection done!!! \n \n");
