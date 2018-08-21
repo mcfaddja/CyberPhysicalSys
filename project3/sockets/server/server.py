09963378de8d9d81d8fb31481f79a03c0e0b5e35
Jonathan McFadden <mcfaddja@uw.edu>
2018-08-20 18:42:39 -0700

    initial receive code for python sockets server

---
+++ project3/sockets/server/server.py
@@ -0,0 +1,15 @@
+import socketserver
+
+class MyIMUdataHandler(socketserver.StreamRequestHandler):
+
+    def handle(self):
+        self.data = self.rfile.readlines().strip()
+        print(self.data)
+        print(type(self.data))
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 5000
+
+    server = socketserver.TCPServer((HOST, PORT), MyIMUdataHandler)
+
+    server.serve_forever()
