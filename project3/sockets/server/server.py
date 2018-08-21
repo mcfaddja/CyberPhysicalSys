3f108ca72f15bcebbec04438bc2f6f935e4a055f
Jonathan McFadden <mcfaddja@uw.edu>
2018-08-20 18:46:53 -0700

    add initialization text to python sockets server code

--- project3/sockets/server/server.py
+++ project3/sockets/server/server.py
@@ -10,6 +10,8 @@ class MyIMUdataHandler(socketserver.StreamRequestHandler):
 if __name__ == "__main__":
     HOST, PORT = "localhost", 5000
 
+    print("started server!")
+    
     server = socketserver.TCPServer((HOST, PORT), MyIMUdataHandler)
 
     server.serve_forever()
