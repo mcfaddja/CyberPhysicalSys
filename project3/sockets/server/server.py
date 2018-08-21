d05939bea0982e0af99fe850a592a9ec4a979b89
Jonathan McFadden <mcfaddja@uw.edu>
2018-08-20 19:06:16 -0700

    added time stamp stuff to python socket server

--- project3/sockets/server/server.py
+++ project3/sockets/server/server.py
@@ -1,17 +1,24 @@
 import socketserver
+import time
+from datetime import datetime
+
+n = 0
 
 class MyIMUdataHandler(socketserver.StreamRequestHandler):
 
     def handle(self):
-        self.data = self.rfile.readlines().strip()
+        myTime = datetime.now()
+        print(myTime.year, myTime.month, myTime.day, myTime.hour, myTime.minute, myTime.second, myTime.microsecond)
+        # self.data = self.request.recv(1024).strip()
+        self.data = self.rfile.readlines()
         print(self.data)
         print(type(self.data))
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 5000
+    HOST, PORT = "192.168.1.40", 5000
 
     print("started server!")
-    
+
     server = socketserver.TCPServer((HOST, PORT), MyIMUdataHandler)
 
     server.serve_forever()
